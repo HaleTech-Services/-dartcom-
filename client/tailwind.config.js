3b2c882b9ee9752733fc3fc2c82ea8b2bb939b1d
Richard-Raph <richardakpan77@gmail.com>
2024-05-23 00:03:31 +0100

    "created dartcom react project with vite and tailwindcss and configured tailwind settings, installed react-icons and react-router-dom for routing"

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,s,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
