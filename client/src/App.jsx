d7f323da7722dead25cf8ea3808f7cbbc3f45590
Akintademuyiwa24 <haakintade@gmail.com>
2024-05-23 05:56:26 -0700

    Add about page with its components and assets, integrate Poppins font,and update dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
-
+import AboutUsPage from "./pages/AboutPage";
 function App() {
-
   return (
     <>
-      <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+      {/* <div className='min-h-screen flex items-center justify-center bg-gray-100'>
         <h1 className='text-3xl font-bold text-blue-500'>
           Hey there 🙋🏿‍♀️🙋🏿‍♂️ Developers 💻🖥⌨🖲, Tailwind CSS with React and Vite! 
         </h1>
+      </div> */}
+      <div>
+        <AboutUsPage />
       </div>
     </>
-  )
+  );
 }
 
 export default App;
---
+++ client/src/pages/AboutPage.jsx
@@ -0,0 +1,46 @@
+import Gallery from "../components/GallerySection";
+import Hero from "../components/HeroSection";
+
+//About us page function
+export default function AboutUsPage() {
+  return (
+    <>
+      <div>
+        {/* render Hero Section */}
+
+        <Hero />
+
+        <div className="m-20">
+          {/* description text */}
+          <div className="m-[30px]">
+            <p>
+              Dartcom Projects Nigeria Limited is owned and managed by a group
+              of indigenous professionals and seasoned Engineers who are experts
+              across the Engineering and management discipline with proven
+              records and experience.
+            </p>
+            <p className="mt-4">
+              Dartcom Projects Nigeria Limited was registered in Nigeria since
+              the year 2009 and has made tremendous achievements and
+              contributions in the field of Engineering. Her staff strength is
+              made up of qualified Civil and Mechanical Engineers, seasoned
+              inspectors and managers Accountants and administrators with many
+              years of experience in their various area of specialization.
+            </p>
+            <p className="mt-4">
+              Dartcom Projects Nigeria Limited has the proven expertise to
+              successfully execute any Engineering and Maintenance Project; the
+              company maintains a system of regular professional and safety
+              performance with adequate safety consciousness among its staff.
+              Dartcom Projects Nigeria Limited terms and conditions are flexible
+              and negotiable.
+            </p>
+          </div>
+
+          {/* render gallery section */}
+          <Gallery />
+        </div>
+      </div>
+    </>
+  );
+}
