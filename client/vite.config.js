9ecbf7ffa4ceee1f71e9b74177ac9c89e2d110b5
Akintademuyiwa24 <haakintade@gmail.com>
2024-05-23 12:43:00 -0700

    confirm changes in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
