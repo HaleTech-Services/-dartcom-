4aee0d4ff2241a874939fd85d2a75924194a2d53
Richard-Raph <richardakpan77@gmail.com>
2024-06-07 05:03:37 +0100

    "Added external dependency to vite.config.js and updated import statement"

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,17 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
     },
   },
 });
