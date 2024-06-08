3f615cb0ac43afc89038d87869285ef74fc76e70
inspikalu <inspikalu@gmail.com>
2024-06-08 18:50:30 +0100

    Gallery number increased

--- client/src/routes/Gallery.jsx
+++ client/src/routes/Gallery.jsx
@@ -25,13 +25,15 @@ export default function Gallery() {
         <button className="ml-auto mr-4 ">Contact Us</button>
       </header>
       <main>
-        <section className="w-full max-h-[10rem] overflow-hidden flex justify-center items-center relative">
-          <img src={constructionImage} alt="" className="w-full h-auto " />
-          <div className="w-full h-full bg-primaryColor absolute top-0 bottom-0 left-0 right-0 opacity-45 flex justify-center items-center">
-            <span className="color-white text-[3rem] ">Gallery</span>
+        <section className="w-full max-h-[10rem] overflow-hidden relative flex justify-center items-center">
+          <div className="w-full h-full absolute top-0 bottom-0 left-0 right-0 bg-blue-500 opacity-50 flex justify-center items-center">
+            <span className="text-white text-3xl">Gallery</span>
           </div>
+          <img src={constructionImage} alt="" className="w-full h-auto object-cover" />
         </section>
-        <div className="grid grid-cols-3 md:grid-cols4 justify-center justify-items-center items-center gap-2">
+        <div className="grid grid-cols-3 md:grid-cols-4 justify-center justify-items-center items-center gap-2 px-[3rem]">
+          <Card />
+          <Card />
           <Card />
           <Card />
           <Card />
@@ -43,34 +45,10 @@ export default function Gallery() {
           <Card />
           <Card />
         </div>
+        <div className="w-full flex items-center justify-center mt-11">
+          <button className="bg-blue-700 text-white py-3 px-[4rem] cursor-pointer rounded-lg">See more</button>
+        </div>
       </main>
-      <footer></footer>
     </>
   );
 }
-
-/*  <header className="w-full bg-white flex justify-center items-center">
-        <div>
-          <ul className="flex">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About Us</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Gallery</a>
-            </li>
-          </ul>
-          <button>Contact Us</button>
-        </div>
-      </header>
-      <section className="body">
-        <img src={constructionImage} alt="" />
-      </section>*/
