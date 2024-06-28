fd41d2f9f64844407dc1b5070d18b256bb73876b
asimic-james <jayasimic@gmail.com>
2024-06-28 16:01:38 +0100

    Contact UI

--- client/src/routes/Contact.jsx
+++ client/src/routes/Contact.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-// import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import belief from "../assets/belief.png";
+import contact_icon from "../assets/undraw_contact_us_re_4qqt.svg";
 import visionImage from "../assets/vision.png";
 import facebook from "../assets/facebook.png";
 import pinterest from "../assets/pinterest.png";
@@ -13,7 +12,7 @@ import mail_icon from "../assets/mail_icon.svg";
 const Contact = () => {
     
     const MailtoLink = ({ emailAddress, label }) => (
-        <a href={`mailto:${emailAddress}`} className="decoration-transparent">
+        <a href={`mailto:${emailAddress}`} className="">
           {label}
         </a>
     );
@@ -31,26 +30,26 @@ const Contact = () => {
           <img src={visionImage} alt="" className="w-full h-auto object-cover" />
         </section>
 
-        <div className="flex items-center justify-center gap-10 bg-gray-200 p-7 pb-10 rounded-lg shadow-lg my-20 mx-auto sm:w-full md:w-3/4 lg:w-1/2">
+        <div className="flex items-center justify-center gap-10 bg-gray-200 p-7 pb-10 rounded-lg shadow-lg my-14 mx-auto sm:w-full md:w-3/4 lg:w-1/2">
             <div className="grid bg-white p-3 rounded-lg shadow-lg gap-4 sm:w-full md:w-full lg:w-full">
                 <div className="flex items-center justify-between">
                     
-                    <div className="m-[20px]">
-                        <img className="w-full h-auto object-cover" src={belief} alt="vision" />
+                    <div className="rounded-full">
+                        <img className="" src={contact_icon} alt="contact" />
                     </div>
 
-                        <form className="grid grid-rows-4 gap-3 h-auto bg-indigo-600 text-white p-3 rounded-lg shadow-lg sm:max-w-[768px] md:w-3/4 lg:w-4/5">  
-                            <label htmlFor="" className="col-span-full text-lg text-white select-none">Your Name</label>
+                        <form className="grid grid-rows-4 gap-3 h-auto bg-gray-200 p-3 rounded-lg shadow-lg sm:max-w-[768px] md:w-3/4 lg:w-4/5">  
+                            <label htmlFor="" className="col-span-full text-lg text-black select-none">Your Name</label>
                             <input type="text" className="col-span-full border rounded-md p-2" placeholder="Enter your name" name="name" />
-                            <label htmlFor="" className="col-span-full text-lg text-white select-none">Your Email</label>
+                            <label htmlFor="" className="col-span-full text-lg text-black select-none">Your Email</label>
                             <input type="text" className="col-span-full border rounded-md p-2" placeholder="Enter your email" name="email" />
-                            <label htmlFor="" className="col-span-full text-lg text-white select-none">Enter your message</label>
+                            <label htmlFor="" className="col-span-full text-lg text-black select-none">Enter your message</label>
                             <textarea className="col-span-full border rounded-md p-3" name="message" rows='3' placeholder='Enter your message...'></textarea>
                             <button type='submit' className="col-span-full bg-[#0029FF] hover:bg-white shadow-lg font-poppins text-white hover:text-[#0029FF] py-2 rounded-md text-center select-none">Submit Now</button>
                         </form>           
                 </div>
-                        <div className="flex items-center justify-evenly gap-4 bg-indigo-600 rounded-md sm:gap-6 md:gap-8 lg:gap-10">
-                            <div className="flex items-center">
+                        <div className="flex items-center justify-evenly gap-4 bg-[#0029FF] rounded-md sm:gap-6 md:gap-8 lg:gap-10">
+                            <div className="flex items-center gap-3">
                                 <img className="my-5 cursor-pointer" src={mail_icon} />
                                 <p className="font-poppins text-white text-lg">oge.akomah@dartcomprojects.com</p>
                             </div>
