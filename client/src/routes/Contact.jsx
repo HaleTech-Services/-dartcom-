d7118b4f3ea130dd189c29b7e5e9afb6535f7ee4
asimic-james <jayasimic@gmail.com>
2024-06-29 10:36:23 +0100

    Hover effects on Submit button

--- client/src/routes/Contact.jsx
+++ client/src/routes/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
 
             <HeroSection p={p} />
 
-            <div className="flex items-center justify-center gap-10 bg-gray-200 p-7 pb-10 rounded-lg shadow-lg my-14 mx-auto sm:w-full md:w-3/4 lg:w-1/2">
+            <section className="flex items-center justify-center gap-10 bg-gray-200 p-7 pb-10 rounded-lg shadow-lg my-14 mx-auto sm:w-full md:w-3/4 lg:w-1/2">
                 <div className="grid bg-white p-3 rounded-lg shadow-lg gap-4 sm:w-full md:w-full lg:w-full">
                     <div className="flex items-center justify-between">
 
@@ -37,20 +37,20 @@ const Contact = () => {
                                 <input type="text" className="col-span-full border rounded-md p-2" placeholder="Enter your email" name="email" />
                                 <label htmlFor="" className="col-span-full text-lg text-black select-none">Enter your message</label>
                                 <textarea className="col-span-full border rounded-md p-3" name="message" rows='3' placeholder='Enter your message...'></textarea>
-                                <button type='submit' className="col-span-full bg-[#0029FF] hover:bg-white shadow-lg font-poppins text-white hover:text-[#0029FF] py-2 rounded-md text-center select-none">Submit Now</button>
+                                <button type='submit' className="hover:duration-700 ease-in-out bg-[#0029FF] hover:bg-white shadow-lg font-poppins text-white hover:text-[#0029FF] py-2 rounded-md text-center font-semibold">Submit</button>
                             </form>           
                     </div>
                             <div className="flex items-center justify-evenly gap-4 bg-[#0029FF] rounded-md sm:gap-6 md:gap-8 lg:gap-10">
-                                <div className="flex items-center gap-3">
-                                    <img className="my-5 w-7 h-7 cursor-pointer" src={mail_icon} />
-                                    <p className="font-poppins text-white text-lg">oge.akomah@dartcomprojects.com</p>
+                                <div className="flex items-center gap-3 cursor-pointer">
+                                    <img className="my-5 w-7 h-7" src={mail_icon} />
+                                    <p className="font-poppins text-white text-lg italic">oge.akomah@dartcomprojects.com</p>
                                 </div>
                                 <img className="my-5 cursor-pointer" src={facebook} alt="facebook" />
                                 <img className="my-5 cursor-pointer" src={linkedin} alt="linkedin" />
                                 <img className="my-5 cursor-pointer" src={pinterest} alt="pinterest" />
                             </div> 
                 </div>
-            </div>
+            </section>
 
             <footer>
                 <Footer />
