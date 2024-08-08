fe6deef70f0a96b2dff506e71ca42e3214371f77
O'Hale <ahamba.ohale@gmail.com>
2024-08-08 18:57:51 +0100

    "Updated HomeBanner and Contact components with changes to layout, styling, and content."

--- client/src/components/HomeBanner.jsx
+++ client/src/components/HomeBanner.jsx
@@ -4,12 +4,12 @@ import smallImg from "../assets/smallImg.png";
 export default function HomeBanner() {
   return (
     <div className="relative w-full mb-[5rem]">
-      <div className="w-full h-fit my-[3rem] md:h-[80vh] flex justify-between">
-        <div className="heroImage w-full overflow-hidden">
-          <img src="/assets/heroimg.jpeg" alt="Hero Image" />
-          <div class="hero-text flex flex-col items-center justify-center gap-3">
-            <p className="capitalize">Welcome to dartcom company</p>
-            <h2 className="capitalize text-4xl font-extrabold max-w-[30ch] text-center">
+      <div className="w-full h-[100vh] flex justify-between my-[3rem] md:h-[80vh]">
+        <div className="heroImage w-full overflow-hidden h-full">
+          <img src="/assets/construction.jpg" alt="Hero Image" className="w-full h-full object-cover" />
+          <div className="hero-text flex flex-col items-center justify-center gap-3">
+            <p className="capitalize text-lg md:text-xl">Welcome to dartcom company</p>
+            <h2 className="capitalize text-2xl md:text-4xl font-extrabold max-w-[30ch] text-center">
               The Best engineering Construction company
             </h2>
             <span className="cursor-pointer bg-white p-3 rounded-sm text-[#4D69FF]">
@@ -18,28 +18,28 @@ export default function HomeBanner() {
           </div>
         </div>
       </div>
-      <div className="info-container w-[90%] md:w-[60%] bg-white absolute -bottom-[20%] md:-bottom-[10%] left-1/2 -translate-x-1/2 bottom-shadow flex items-center justify-between p-8 mb-3 translate-y-4 md:translate-y-0 gap-3">
+      <div className="info-container w-[90%] md:w-[60%] bg-white absolute -bottom-[20%] md:-bottom-[10%] left-1/2 -translate-x-1/2 bottom-shadow flex items-center justify-between p-4 md:p-8 mb-3 translate-y-4 md:translate-y-0 gap-3">
         <div className="info-item flex flex-col md:flex-row items-center justify-center">
-          <span className="info-number text-[#FF7B00] text-2xl font-bold">
+          <span className="info-number text-[#FF7B00] text-xl md:text-2xl font-bold">
             25+
           </span>
-          <span className="info-text text-sm md:text-base">
+          <span className="info-text text-xs md:text-base">
             Years of experience
           </span>
         </div>
         <div className="info-item flex flex-col md:flex-row items-center justify-center">
-          <span className="info-number text-[#FF7B00] text-2xl font-bold">
+          <span className="info-number text-[#FF7B00] text-xl md:text-2xl font-bold">
             160+
           </span>
-          <span className="info-text text-sm md:text-base">
+          <span className="info-text text-xs md:text-base">
             Completed Projects
           </span>
         </div>
         <div className="info-item flex flex-col md:flex-row items-center justify-center">
-          <span className="info-number text-[#FF7B00] text-2xl font-bold">
+          <span className="info-number text-[#FF7B00] text-xl md:text-2xl font-bold">
             900+
           </span>
-          <span className="info-text text-sm md:text-base">
+          <span className="info-text text-xs md:text-base">
             Customer reviews
           </span>
         </div>
--- client/src/routes/Contact.jsx
+++ client/src/routes/Contact.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
           <div className="mt-8">
             <div className="relative h-64 w-full">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2441.678031899069!2d-0.1399628237809214!3d51.53230101386771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761b1c1a0eeca1%3A0x9c9de78e076869f8!2sZSL%20London%20Zoo!5e0!3m2!1sen!2suk!4v1620280218248!5m2!1sen!2suk"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.8809209757567!2d7.028004014766333!3d4.866690096445476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1069cde89137f0f9%3A0xe4c9a6f0f8f6d9cb!2s19B%20Eliwelibo%20Main%20Rd%2C%20Mandela%20Estate%2C%20Rukpokwu%2C%20Port%20Harcourt!5e0!3m2!1sen!2sng!4v1691498366842!5m2!1sen!2sng"
                 title="Map"
                 className="absolute inset-0 w-full h-full border-0"
                 allowFullScreen=""
@@ -74,25 +74,33 @@ const Contact = () => {
             <span className="block mt-5">Get a quote</span>
             <h2 className="text-2xl font-bold">Build Your Future Today</h2>
             <h3>LET DARTCOM BRING THAT FUTURE OUT TODAY</h3>
-            <button className="bg-white p-3 rounded-md text-darkBlue mt-3 mb-auto">Contact Us Now</button>
+            <button className="bg-white p-3 rounded-md text-darkBlue mt-3 mb-auto">
+              Contact Us Now
+            </button>
 
             <section className="flex gap-3 text-black flex-col md:flex-row mb-3">
               <div className="flex flex-row gap-2 bg-white items-center justify-start md:justify-center p-3">
-                <span className="bg-red-400 text-white font-bold text-2xl p-3">01</span>
+                <span className="bg-red-400 text-white font-bold text-2xl p-3">
+                  01
+                </span>
                 <div className="flex flex-col">
                   <span>Best Consultation</span>
                   <span>We got the best on Consultation</span>
                 </div>
               </div>
               <div className="flex gap-2 bg-white items-center justify-center p-3">
-                <span className="bg-red-400 text-white font-bold text-2xl p-3">02</span>
+                <span className="bg-red-400 text-white font-bold text-2xl p-3">
+                  02
+                </span>
                 <div className="flex flex-col">
                   <span>Best Custormer Review</span>
                   <span>We got reviews Everyday</span>
                 </div>
               </div>
               <div className="flex gap-2 bg-white items-center justify-center p-3">
-                <span className="bg-red-400 text-white font-bold text-2xl p-3">03</span>
+                <span className="bg-red-400 text-white font-bold text-2xl p-3">
+                  03
+                </span>
                 <div className="flex flex-col">
                   <span>Best Maintenance Service</span>
                   <span>Our Maintenance is Top #1</span>
